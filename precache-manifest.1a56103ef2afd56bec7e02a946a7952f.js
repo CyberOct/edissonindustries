12af57d4d433108077197344c8f5c995e68db817
Stefan Butura <stefan.butura@gmail.com>
2020-10-27 18:59:15 +0200

    Updates

---
+++ precache-manifest.1a56103ef2afd56bec7e02a946a7952f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750a64311bc545480cfabffac04bfb6e",
+    "url": "/Edisson-Industries/index.html"
+  },
+  {
+    "revision": "09e0375a3b9a470f86a3",
+    "url": "/Edisson-Industries/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8b056abf8a206aebd85d",
+    "url": "/Edisson-Industries/static/css/main.f58cf40d.chunk.css"
+  },
+  {
+    "revision": "09e0375a3b9a470f86a3",
+    "url": "/Edisson-Industries/static/js/2.51a1e980.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Edisson-Industries/static/js/2.51a1e980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b056abf8a206aebd85d",
+    "url": "/Edisson-Industries/static/js/main.a1554226.chunk.js"
+  },
+  {
+    "revision": "62e1bf8d71fa4dafd1fa",
+    "url": "/Edisson-Industries/static/js/runtime-main.56e70478.js"
+  },
+  {
+    "revision": "46a445d5dd9348a1f547d7e441135d7a",
+    "url": "/Edisson-Industries/static/media/EdissonTitle.46a445d5.svg"
+  },
+  {
+    "revision": "f2f4a953f6b611aff2fa3b21f759680a",
+    "url": "/Edisson-Industries/static/media/FAFERS_Technical_Font.f2f4a953.ttf"
+  },
+  {
+    "revision": "5c42d8054ee2009128a3adbc5853f3d0",
+    "url": "/Edisson-Industries/static/media/Logo2Update.5c42d805.png"
+  },
+  {
+    "revision": "fdcf8891970918ef12fbd23153aa2f39",
+    "url": "/Edisson-Industries/static/media/background-1.fdcf8891.jpg"
+  },
+  {
+    "revision": "806c761db7227ca8d1fb158574fe1c39",
+    "url": "/Edisson-Industries/static/media/electric-1.806c761d.jpg"
+  },
+  {
+    "revision": "88da433fb342dea56132128bf849cbe0",
+    "url": "/Edisson-Industries/static/media/electric-13.88da433f.jpg"
+  },
+  {
+    "revision": "ef30ddea428d20436dfda8a950a0cd89",
+    "url": "/Edisson-Industries/static/media/electric-14.ef30ddea.jpg"
+  },
+  {
+    "revision": "a0ae411cf8e2d2d07de3b1d91bd360bd",
+    "url": "/Edisson-Industries/static/media/electric-15.a0ae411c.jpg"
+  },
+  {
+    "revision": "86010e23587bd21301be784e6270503c",
+    "url": "/Edisson-Industries/static/media/electric-16.86010e23.jpg"
+  },
+  {
+    "revision": "d335aea97f954b50a00dabaa16146604",
+    "url": "/Edisson-Industries/static/media/electric-19.d335aea9.jpg"
+  },
+  {
+    "revision": "fc2ff5deddcaf2868f5d5a5486a6a218",
+    "url": "/Edisson-Industries/static/media/electric-2.fc2ff5de.jpg"
+  },
+  {
+    "revision": "3130ffa3603a8791607ee7244924a4c7",
+    "url": "/Edisson-Industries/static/media/electric-20.3130ffa3.jpg"
+  },
+  {
+    "revision": "aeb29d79d37f34169483508ac3625ee0",
+    "url": "/Edisson-Industries/static/media/electric-21.aeb29d79.jpg"
+  },
+  {
+    "revision": "4db0857c143bb9aa152cba76ee8ed352",
+    "url": "/Edisson-Industries/static/media/electric-22.4db0857c.jpg"
+  },
+  {
+    "revision": "de56e74720ef96e0e6f14c3f5ec67969",
+    "url": "/Edisson-Industries/static/media/electric-3.de56e747.jpg"
+  },
+  {
+    "revision": "6cf327c623fde1f70c37070a56ed79c4",
+    "url": "/Edisson-Industries/static/media/electric-4.6cf327c6.jpg"
+  },
+  {
+    "revision": "6cb3fccebc521a4b2d4b9904244823e4",
+    "url": "/Edisson-Industries/static/media/electric-6.6cb3fcce.jpg"
+  },
+  {
+    "revision": "72ab9f3ede86b7151b9c448a94ebb91b",
+    "url": "/Edisson-Industries/static/media/electric-7.72ab9f3e.jpg"
+  },
+  {
+    "revision": "a0782ac03bbe9f32c1d6b36fd3797336",
+    "url": "/Edisson-Industries/static/media/electric-8_resize.a0782ac0.jpg"
+  },
+  {
+    "revision": "1f3b23e1d3bd33dc58c4b421d71bf32d",
+    "url": "/Edisson-Industries/static/media/electric-9.1f3b23e1.jpg"
+  },
+  {
+    "revision": "16ea262ca412ff0fd2c13c6f60eaa3f7",
+    "url": "/Edisson-Industries/static/media/small-Hero-4.16ea262c.jpg"
+  },
+  {
+    "revision": "306ce520b2d527afe04227aca21a454b",
+    "url": "/Edisson-Industries/static/media/small-Hero-7.306ce520.jpg"
+  },
+  {
+    "revision": "d9c7c7d3a644b5723536bfd8f3cb3ee7",
+    "url": "/Edisson-Industries/static/media/small-Hero-8.d9c7c7d3.jpg"
+  },
+  {
+    "revision": "8254ee71d0e1c1a0ae47079257e1212b",
+    "url": "/Edisson-Industries/static/media/small-Hero-9.8254ee71.jpg"
+  }
+]);
